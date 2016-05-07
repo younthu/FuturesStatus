1cdbef9d1b4fb1c1902cbbac9e0b069d87bb0594
Andrew(Zhiyong) Yang <evil_eve_live@outlook.com>
2016-05-07 19:45:51 +0800

    It pulls AG1606 correctly now.

--- FuturesStatus/FuturesStatus/AppDelegate.swift
+++ FuturesStatus/FuturesStatus/AppDelegate.swift
@@ -7,6 +7,10 @@
 //
 
 import Cocoa
+import Alamofire
+
+let UP_CHAR="↓"
+let DOWN_CHAR="↑"
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
@@ -14,14 +18,16 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     @IBOutlet weak var window: NSWindow!
 
     
-    let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(-2)
+    let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(-1)
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
 //        let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(-2)
         
         if let button = statusItem.button {
-            button.image = NSImage(named: "StatusBarButtonImage")
+//            button.image = NSImage(named: "StatusBarButtonImage")
+            button.title = "AG1606:3753";
             button.action = Selector("printQuote:")
+            button
         }
         
         let menu = NSMenu()
@@ -32,8 +38,42 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         
         statusItem.menu = menu
         
+        startNetworkRequest(10);
     }
     
+    func startNetworkRequest(intervalSecs:UInt64) -> Void {
+        // use sina api, ref: http://blog.sina.com.cn/s/blog_7ed3ed3d0101gphj.html
+        Alamofire.request(.GET, "http://hq.sinajs.cn/list=AG1606", parameters: nil)
+            .responseString { response in
+                
+                dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (Int64)(intervalSecs * NSEC_PER_SEC)), dispatch_get_main_queue(), {
+                    self.startNetworkRequest(intervalSecs);
+                })
+                
+                if  response.result.isFailure {
+                    return;
+                }
+                
+                
+//                println(string)
+                let myStringToBeMatched = response.result.value
+                let myRegex = "\".*\""
+                if let match = myStringToBeMatched!.rangeOfString(myRegex, options: .RegularExpressionSearch){
+                    print("\(myStringToBeMatched) is matching!")
+                    
+                    let values = myStringToBeMatched?.substringWithRange(match).componentsSeparatedByString(",")
+                    let str = NSString(format: "%.0f,%@ :%@", (NSDate().timeIntervalSinceReferenceDate), values![0], values![8]) as String;
+//                    self.statusItem.button?.title =  str //values![0] + (values![8])
+                    
+                    if let button = self.statusItem.button {
+                        //            button.image = NSImage(named: "StatusBarButtonImage")
+                        button.title = str;
+                    }
+                }
+                
+            }
+        
+    }
     func printQuote(sender: AnyObject) {
         let quoteText = "Never put off until tomorrow what you can do the day after tomorrow."
         let quoteAuthor = "Mark Twain"
